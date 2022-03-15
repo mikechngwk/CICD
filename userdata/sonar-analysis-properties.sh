540309eb513cd1213a37cb1cb2369fdc2be3c1cf
mikechngwk <mikechngwk@gmail.com>
2022-03-15 10:10:38 +0800

    added sonar-application-properties

---
+++ userdata/sonar-analysis-properties.sh
@@ -0,0 +1,9 @@
+
+sonar.projectKey=cicd
+sonar.projectName=cicd-repo
+sonar.projectVersion=1.0
+sonar.sources=src/
+sonar.java.binaries= "to be filled up by developer"
+sonar.junit.reportsPath=target/surefire-reports/
+sonar.jacoco.reportsPath=target/jacoco.exec
+sonar.java.checkstyle.reportPaths=target/checkstyle-result.xml
